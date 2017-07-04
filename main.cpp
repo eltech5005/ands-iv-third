03f286a14445f22cc9ce856ee89480976447634d
iAdanos <iadanos@yandex.com>
2017-07-04 22:39:00 +0300

    Задание

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
-#include <stdio.h>
+/* -----
+ * Вариант: 5
+ * Вид дерева: Двоичное
+ * Разметка: Симметричная
+ * Способ обхода: В ширину
+ * Задание: Количество вершин, имеющих не более одного потомка.
+ ----- */
 
-int main(int argc, char **argv)
-{
-	printf("hello world\n");
-	return 0;
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+    cout << "hello world\n";
+
+    return 0;
 }
